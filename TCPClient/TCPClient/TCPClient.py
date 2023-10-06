11a8700d85505cb7b7d8c3be84f044bc8020b67a
Oliver Mikkel Opfermann Göth <oliv8037@edu.zealand.dk>
2023-10-06 11:29:40 +0200

    changed servername to "localhost"

--- TCPClient/TCPClient/TCPClient.py
+++ TCPClient/TCPClient/TCPClient.py
@@ -1,6 +1,6 @@
 from socket import *
 
-serverName = '127.0.0.1'
+serverName = 'localhost'
 serverPort = 12000
 clientSocket  = socket(AF_INET,SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
